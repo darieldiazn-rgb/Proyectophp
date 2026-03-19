796e32f75a097c6d1c2e501a04540f18e336c586
Darieel <darieldiazn@gmail.com>
2026-03-19 14:40:13 -0400

    index falso

--- index.php
+++ index.php
@@ -13,9 +13,11 @@
     <?php 
 $fecha = date("d-m-y");
 $nombre = "Dariel";
-echo "<h1>Hola Mundo desde PHP</h1>";
+echo "<h1>Hoaa Mundo desde PHP \"alo\"</h1>";
 echo "<p>Soy <strong>".$nombre."</strong> y hoy es ".$fecha."</p>";
 echo "<small>Este es un texto pequeño</small>";
 ?>
+
+
   </body>
 </html>
